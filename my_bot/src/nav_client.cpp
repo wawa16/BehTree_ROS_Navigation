70ab1c95b35edff4298fe4f8f18d0d3dee55dd91
Waleed Uddin <mwaleed16@hotmail.com>
2019-01-17 20:34:44 -0800

    Update nav_client.cpp

--- my_bot/src/nav_client.cpp
+++ my_bot/src/nav_client.cpp
@@ -12,7 +12,7 @@ move_base_msgs::MoveBaseGoal goal;
 int main(int argc, char **argv){
 	ros::init(argc, argv, "MoveBaseGoals");
 	MoveBaseClient client("move_base", true);
-	int waypoints = 3; int b; int c;
+	int waypoints = 3;
     float pose[3][2][4] = {{{1.168, -1.728, 0.0, 0.0}, {0.0, 0.0, -0.812, 0.5820}},
     {{-0.312, -2.581, 0.0, 0.0},{0.0, 0.0, 0.9385, 0.345}},
     {{-0.9699, -0.718, 0.0, 0.0},{0.0, 0.0, 0.5856, 0.8105}}
@@ -41,9 +41,9 @@ int main(int argc, char **argv){
 
 void goalpose(float pose[3][2][4], int &waypoints){
 	
-	goal.target_pose.header.frame_id = "map";
+    goal.target_pose.header.frame_id = "map";
     goal.target_pose.header.stamp = ros::Time::now();
-	goal.target_pose.pose.position.x = pose[waypoints][0][0];
+    goal.target_pose.pose.position.x = pose[waypoints][0][0];
     goal.target_pose.pose.position.y = pose[waypoints][0][1];
     goal.target_pose.pose.position.z = pose[waypoints][0][2];
     goal.target_pose.pose.orientation.z = pose[waypoints][1][2];
